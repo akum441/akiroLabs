022efc570e292f0185a850b498fadbefb9e7baad
Arun Kumar <arun111143@gmail.com>
2023-10-19 00:45:29 +0200

    resolved cors issues

--- generator/src/main/java/com/arun/generator/controllers/TokenGenerationController.java
+++ generator/src/main/java/com/arun/generator/controllers/TokenGenerationController.java
@@ -2,6 +2,7 @@ package com.arun.generator.controllers;
 
 import com.arun.generator.dto.TokenOptions;
 import com.arun.generator.utils.TokenConstants;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Random;
 @RestController
 public class TokenGenerationController {
     private final Random random = new Random();
+
 
     @PostMapping("/generateToken")
     public String generateToken(@RequestBody TokenOptions tokenOptions) {
