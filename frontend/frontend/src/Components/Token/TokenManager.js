6da40fd2833a66545aed7827e3782858ac03a96c
Arun Kumar <arun111143@gmail.com>
2023-10-19 02:58:51 +0200

    cahnged layout of the buttons and make flag user frendly

--- frontend/frontend/src/Components/Token/TokenManager.js
+++ frontend/frontend/src/Components/Token/TokenManager.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import './TokenManager.css';
 import axios from 'axios';
-import {options} from '../../Constants/Values';
-import { ButtonGroup, Button, Row, Col, Card, Form } from 'react-bootstrap';
+import { options } from '../../Constants/Values';
+import { ButtonGroup, Button, Row, Col, Card, Form, InputGroup } from 'react-bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import the icons stylesheet
 
 
 function TokenManager() {
     const [allowedDigits, setAllowedDigits] = useState([]);
     const [token, setToken] = useState('');
-    const [isValid, setIsValid] = useState(null);
+    const [isValid, setIsValid] = useState("Flag Unavailable");
 
     const [isGenerated, setIsGenerated] = useState(false);
     const [isValidated, setIsValidated] = useState(false);
 
+    const [checking, setChecking] = useState(false);
+
     const handleDigitToggle = (digit) => {
+        setIsGenerated(true);
         const newDigits = [...allowedDigits];
         if (newDigits.includes(digit)) {
             setAllowedDigits(newDigits.filter(d => d !== digit));
@@ -23,22 +27,23 @@ function TokenManager() {
         }
     };
 
-    const generateToken = async() => {
+    const generateToken = async () => {
         try {
             const response = await axios.post('http://localhost:8080/generateToken', { options: allowedDigits });
             setToken(response.data);
-            setIsGenerated(true);
+            // setIsGenerated(true);
             setIsValidated(false);
         } catch (error) {
             console.error("Error generating token:", error);
         }
     };
 
-    const validateToken = async() => {
+    const validateToken = async () => {
         try {
             const response = await axios.post('http://localhost:8081/validate', { token });
             setIsValid(response.data);
-            setIsValidated(true);
+            setChecking(true);
+            setTimeout(() => setChecking(false), 500);
         } catch (error) {
             console.error("Error validating token:", error);
         }
@@ -49,77 +54,82 @@ function TokenManager() {
         setAllowedDigits([]);
         setIsGenerated(false);
         setIsValidated(false);
+        setIsValid("Flag Unavailable");
         setToken('');
     };
 
     return (
         <div>
-        <Row>
-            <Col md={12}>
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Choose numbers to generate the token</Card.Title>
-                        <ButtonGroup>
-                            {options.map((_, idx) => (
-                                <Button 
-                                    key={idx} 
-                                    variant={allowedDigits.includes(idx) ? 'primary' : 'outline-primary'}
-                                    onClick={() => handleDigitToggle(idx)}
-                                >
-                                    {idx}
-                                </Button>
-                            ))}
-                        </ButtonGroup>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col md={12}>
-                <Card>
-                    <Card.Body>
-                    <Card.Title>Token Output</Card.Title>
+            <Row>
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title className='Ctitle'>Choose numbers to generate the token</Card.Title>
+                            <ButtonGroup>
+                                {options.map((_, idx) => (
+                                    <Button
+                                        key={idx}
+                                        variant={allowedDigits.includes(idx) ? 'primary' : 'outline-primary'}
+                                        onClick={() => handleDigitToggle(idx)}
+                                    >
+                                        {idx}
+                                    </Button>
+                                ))}
+                            </ButtonGroup>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={6} className="token-container">
+                    <Card>
+                        <Card.Body>
+                            <Card.Title className='Ctitle'>Generated Token</Card.Title>
+                            <InputGroup>
+                                <Form.Control
+                                    type="text"
+                                    value={token}
+                                    onChange={e => setToken(e.target.value)}
+                                    placeholder="Generated Token"
+                                />
+                                <InputGroup.Text>
+                                    <i className="bi bi-pencil"></i>
+                                </InputGroup.Text>
+                            </InputGroup>
 
-                        <Form.Control 
-                            type="text" 
-                            value={token}
-                            onChange={e => setToken(e.target.value)}
-                            placeholder="Generated Token"
-                        />
-                    </Card.Body>
-                </Card>
-            </Col>
-           
-          
-                <div id="flag" className={isValid === true ? 'text-success' : 'text-danger'}>
-                    {isValid === null ? '' : isValid ? 'Valid Token' : 'Invalid Token'}
-                </div>
-        
-        </Row>
-        <Row className="mt-3">
-            <Col>
-                <ButtonGroup>
-                    <Button 
-                        variant={isGenerated ? 'primary' : 'outline-primary'}
-                        onClick={generateToken}
-                    >
-                        Generate
-                    </Button>
-                    <Button 
-                        variant={isValidated ? 'primary' : 'outline-primary'}
-                        onClick={validateToken}
-                        disabled={!isGenerated}
-                    >
-                        Validate
-                    </Button>
-                    <Button 
-                        variant="outline-danger"
-                        onClick={handleReset}
-                    >
-                        Reset
-                    </Button>
-                </ButtonGroup>
-            </Col>
-        </Row>
-    </div>
+                            <span id="token-flag" className={isValid === true ? 'text-success' : 'text-danger'}>
+                                {checking ? 'Checking...' : (isValid === "Flag Unavailable" ? "Token Status unavailabe" : isValid ? 'Valid Token' : 'Invalid Token')}
+                            </span>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+            </Row>
+            <Row className="mt-3">
+                <Col>
+                    <ButtonGroup>
+                        <Button
+                            variant={isGenerated ? 'success' : 'outline-primary'}
+                            onClick={generateToken}
+                            disabled={!isGenerated}
+                        >
+                            Generate
+                        </Button>
+                        <Button
+                            variant={isValidated ? 'primary' : 'outline-primary'}
+                            onClick={validateToken}
+                            disabled={!token}
+                        >
+                            Validate
+                        </Button>
+                        <Button
+                            variant="outline-danger"
+                            onClick={handleReset}
+                        >
+                            Reset
+                        </Button>
+                    </ButtonGroup>
+                </Col>
+            </Row>
+        </div>
     );
 }
 
